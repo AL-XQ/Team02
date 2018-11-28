df4dd394a78a2805ba4fdf5b0aa76b42d484e538
KPC-018A1473\K018A1473 <tsuruwatcher@gmail.com>
2018-11-28 13:50:40 +0900

    Blockと当たったら死亡するKillBlockを追加

---
+++ Team02/Team02/Scene/Stage/GameObjs/Block.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfinityGame.Device;
+using InfinityGame.GameGraphics;
+
+namespace Team02.Scene.Stage.GameObjs
+{
+    class Block : GameObj
+    {
+        public Block(BaseDisplay aParent, string aName) : base(aParent, aName)
+        {
+        }
+
+        public override void PreLoadContent()
+        {
+            IsCrimp = true;
+            base.PreLoadContent();
+        }
+
+        public override void LoadContent()
+        {
+            image = ImageManage.GetSImage("chat.png");
+            base.LoadContent();
+        }
+    }
+}
