0b211aa81d3293b443b104c06edd5f3b3d7cded7
shozero09127 <h311506509@edu.teu.ac.jp>
2018-11-29 13:22:31 +0900

    testdata comentout

--- Team02/Team02/Scene/PlayScene.cs
+++ Team02/Team02/Scene/PlayScene.cs
@@ -37,8 +37,8 @@ namespace Team02.Scene
 
         public override void Initialize()
         {
-            if (aaaaa.Stage.Name == "stage2")
-                aaaaa.ChangeStage(stages["stage1"]);
+            //if (aaaaa.Stage.Name == "stage2")
+            //    aaaaa.ChangeStage(stages["stage1"]);
             ShowStage = stages["stage1"];
             base.Initialize();
         }
@@ -47,40 +47,40 @@ namespace Team02.Scene
         {
             player = new Player();
             new Base_Stage(this, "stage1");
-            new Base_Stage(this, "stage2");
-            aaaaa = new GameObj(stages["stage1"], "aaa");
-            stages["stage1"].stageObjs["aaa"].Size = new Size(200, 200);
-            stages["stage1"].stageObjs["aaa"].Coordinate = new Vector2(200, 200);
-            test = new AnimeButton(this);
-            test.Size = size / 5;
-            test.Location = new Point(500, 500);
+            //new Base_Stage(this, "stage2");
+            //aaaaa = new GameObj(stages["stage1"], "aaa");
+            //stages["stage1"].stageObjs["aaa"].Size = new Size(200, 200);
+            //stages["stage1"].stageObjs["aaa"].Coordinate = new Vector2(200, 200);
+            //test = new AnimeButton(this);
+            //test.Size = size / 10;
+            //test.Location = new Point(500, 500);
 
-            test1 = new AnimeButton(this);
-            test1.Size = size / 5;
-            test1.Location = new Point(1000, 500);
-            test1.Text = "ステージ変更";
-            test2 = new AnimeButton(this);
-            test2.Size = size / 5;
-            test2.Location = new Point(1000, 700);
-            test2.Text = "アイテム転送";
+            //test1 = new AnimeButton(this);
+            //test1.Size = size / 10;
+            //test1.Location = new Point(1000, 500);
+            //test1.Text = "ステージ変更";
+            //test2 = new AnimeButton(this);
+            //test2.Size = size / 10;
+            //test2.Location = new Point(1000, 700);
+            //test2.Text = "アイテム転送";
             //stages["stage1"].FocusStageObj = stages["stage1"].stageObjs["aaa"];
             base.PreLoadContent();
         }
         public override void LoadContent()
         {
-            test.Image = ImageManage.GetSImage("button01");
-            test.Click += test_Click;
-            test1.Image = ImageManage.GetSImage("button01");
-            test1.Click += test1_Click;
-            test2.Image = ImageManage.GetSImage("button01");
-            test2.Click += test2_Click;
+            //test.Image = ImageManage.GetSImage("button01");
+            //test.Click += test_Click;
+            //test1.Image = ImageManage.GetSImage("button01");
+            //test1.Click += test1_Click;
+            //test2.Image = ImageManage.GetSImage("button01");
+            //test2.Click += test2_Click;
             base.LoadContent();
         }
 
         public override void Update(GameTime gameTime)
         {
-            if (ShowStage != null)
-                test.Text = "タイトルに戻る\nNow:" + ShowStage.Name;
+            //if (ShowStage != null)
+            //    test.Text = "タイトルに戻る\nNow:" + ShowStage.Name;
             player.Update(gameTime);
             base.Update(gameTime);
         }
--- Team02/Team02/Scene/Stage/Base_Stage.cs
+++ Team02/Team02/Scene/Stage/Base_Stage.cs
@@ -33,23 +33,22 @@ namespace Team02.Scene.Stage
 
         public override void PreLoadContent()
         {
-            test = new GameObj(this, "aaa2");
-            stageObjs["aaa2"].Size = new Size(200, 200);
-            stageObjs["aaa2"].Coordinate = new Vector2(0, 0);
-            Player.Chara = new Hero(this, "hero");
-            new Enemy(this, "enemy");
-            stageObjs["hero"].Coordinate = new Vector2(1100, 1100);
-            stageObjs["hero"].Size = new Size(100, 100);
-            stageObjs["enemy"].Coordinate = new Vector2(1500, 1100);
-            stageObjs["enemy"].Size = new Size(100, 100);
-            new Block(this, "block");
-            new KillBlock(this, "killblock")
-;
-            stageObjs["block"].Coordinate = new Vector2(500, 1200);
-            stageObjs["block"].Size = new Size(2000, 200);
-            stageObjs["killblock"].Coordinate = new Vector2(1000, 1500);
-            stageObjs["killblock"].Size = new Size(100, 200);
-            FocusStageObj = stageObjs["hero"];
+            //test = new GameObj(this, "aaa2");
+            //stageObjs["aaa2"].Size = new Size(200, 200);
+            //stageObjs["aaa2"].Coordinate = new Vector2(0, 0);
+            //Player.Chara = new Hero(this, "hero");
+            //new Enemy(this, "enemy");
+            //stageObjs["hero"].Coordinate = new Vector2(1100, 1100);
+            //stageObjs["hero"].Size = new Size(100, 100);
+            //stageObjs["enemy"].Coordinate = new Vector2(1500, 1100);
+            //stageObjs["enemy"].Size = new Size(100, 100);
+            //new Block(this, "block");
+            //new KillBlock(this, "killblock");
+            //stageObjs["block"].Coordinate = new Vector2(500, 1200);
+            //stageObjs["block"].Size = new Size(2000, 200);
+            //stageObjs["killblock"].Coordinate = new Vector2(1000, 1500);
+            //stageObjs["killblock"].Size = new Size(100, 200);
+            //FocusStageObj = stageObjs["hero"];
             base.PreLoadContent();
         }
         public override void Update(GameTime gameTime)
