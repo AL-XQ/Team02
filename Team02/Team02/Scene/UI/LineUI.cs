b562e7c03fc26bf9e256e463b72c5e87b12f9938
tatsuya9034 <tatsuya.stoo@gmail.com>
2018-12-13 14:43:50 +0900

    lazerとarrowの画像差し替え

--- Team02/Team02/Scene/UI/LineUI.cs
+++ Team02/Team02/Scene/UI/LineUI.cs
@@ -17,18 +17,49 @@ using Team02.Scene.Stage.GameObjs;
 
 namespace Team02.Scene.UI
 {
-    public class LineUI : Label
+    public class LineUI : Panel
     {
         private PlayScene playScene;
         private Point playerPosition;
         private Point mousePosition;
+
         private float rotation;
-        private Texture2D point;
+        private string imageName;
+
+        private bool isShoot = true;
+
+        public bool IsShoot { get => isShoot; set => SetIsShoot(value); }
+
+        public string ImageName { get => imageName; set => SetImageName(value); }
 
         public LineUI(BaseDisplay parent) : base(parent)
         {
             //Color = Color.Transparent;
             playScene = (PlayScene)parent;
+
+
+        }
+
+        private void SetImageName(string value)
+        {
+            imageName = value;
+            if (ImageName != null)
+            {
+                image = ImageManage.GetSImage(imageName);
+            }
+        }
+
+        private void SetIsShoot(bool value)
+        {
+            isShoot = value;
+            if (isShoot)
+            {
+                ImageName = "lazer_test.png";
+                Size = new Size(400, image.Size.Height);
+                return;
+            }
+            ImageName = "arrow_test.png";
+            Size = new Size(image.Size.Width * 3 / 4, image.Size.Height);
         }
 
         public override void PreLoadContent()
@@ -38,9 +69,7 @@ namespace Team02.Scene.UI
 
         public override void LoadContent()
         {
-            //Image = 
-            point = new Texture2D(graphicsDevice, 1, 1);
-            point.SetData<Color>(new Color[] { Color.Blue });
+            IsShoot = true;
             base.LoadContent();
         }
 
@@ -48,18 +77,20 @@ namespace Team02.Scene.UI
         {
             playerPosition = playScene.GetDrawLocation(playScene.Player.Chara.ISpace.Center);
             mousePosition = playScene.GameRun.GameMouse.MouseState.Position;
-            //var temp0 = playerPosition - mousePosition;
-            //var temp1 = temp0.ToVector2().Length();
-            size = new Size(400, 3);
+            var temp0 = playerPosition - mousePosition;
+            var temp1 = temp0.ToVector2().Length();
+            if (isShoot)
+                Size = new Size(temp1, image.Size.Height);
 
             Vector2 direction = mousePosition.ToVector2() - playerPosition.ToVector2();
             rotation = (float)Math.Atan2(direction.Y, direction.X);
+
             base.Update(gameTime);
         }
 
-        public override void Draw2(GameTime gameTime)
+        public override void Draw1(GameTime gameTime)
         {
-            spriteBatch.Draw(point, new Rectangle(playerPosition, Size.ToPoint()), new Rectangle(Point.Zero, new Point(1, 1)), imageColor, rotation, Vector2.Zero, SpriteEffects.None, 1f);
+            spriteBatch.Draw(image.ImageT[iTIndex], new Rectangle(playerPosition, Size.ToPoint()), new Rectangle(Point.Zero, Image.Size.ToPoint()), imageColor, rotation, new Vector2(0, image.Size.Height / 2), SpriteEffects.None, 1f);
             //base.Draw2(gameTime);
         }
     }
