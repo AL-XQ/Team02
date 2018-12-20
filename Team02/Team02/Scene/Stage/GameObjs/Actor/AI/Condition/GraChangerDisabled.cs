4faab446e96ebd81170af69edd569ed4c1bc2e4e
鶴見昌之 <tsuruwatcher@gmail.com>
2018-12-20 14:17:59 +0900

    一旦保存

---
+++ Team02/Team02/Scene/Stage/GameObjs/Actor/AI/Condition/GraChangerDisabled.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team02.Scene.Stage.GameObjs.Actor.AI.Condition
+{
+    class GraChangerDisabled : ConditionBase
+    {
+        public GraChangerDisabled(GraChangerDisabled sample) : base(sample)
+        {
+
+        }
+
+        public override bool Check()
+        {
+            return User.GraChanger != null;
+        }
+
+        public override ConditionBase Copy()
+        {
+            return new GraChangerDisabled(this);
+        }
+    }
+}
