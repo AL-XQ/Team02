ef6ddf084a387755484c0b37699ed007e1273da0
shozero09127 <h311506509@edu.teu.ac.jp>
2019-01-09 11:01:47 +0900

    HiddenBlockクラスの追加

---
+++ Team02/Team02/Scene/Stage/GameObjs/HiddenBlock.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfinityGame.GameGraphics;
+using InfinityGame.Stage;
+using InfinityGame.Stage.StageObject;
+using InfinityGame.Element;
+using Microsoft.Xna.Framework;
+
+using Team02.Scene.Stage.GameObjs.Actor;
+
+namespace Team02.Scene.Stage.GameObjs
+{
+    public class HiddenBlock : LoopedBlock
+    {
+        private bool isPush;
+        public HiddenBlock(BaseDisplay aParent, string aName) : base(aParent, aName)
+        {
+            IsCrimp = true;
+            isPush = false;
+        }
+
+        public HiddenBlock(MapCreator mapCreator, Dictionary<string, object> args) : base(mapCreator, args)
+        {
+            IsCrimp = true;
+        }
+
+        public override void CalCollision(StageObj obj)
+        {
+            if (obj is Hero)
+            {
+                isPush = true;
+            }
+            base.CalCollision(obj);
+        }
+        public override void PreLoadContent()
+        {
+            base.PreLoadContent();
+        }
+        public override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void SetImage()
+        {
+            ImageName = "bullet_test.png";
+        }
+        public override void Update(GameTime gameTime)
+        {
+            if (isPush)
+            {
+                Kill();
+            }
+            base.Update(gameTime);
+        }
+    }
+}
--- Team02/Team02/Scene/Stage/Stage01.cs
+++ Team02/Team02/Scene/Stage/Stage01.cs
@@ -19,6 +19,7 @@ namespace Team02.Scene.Stage
     public class Stage01 : Base_Stage
     {
         private Enemy testenemy;
+        private HiddenBlock testbutton;
         public Stage01(BaseDisplay aParent, string aName) : base(aParent, aName)
         {
             EndOfRightDown = new Vector2(2000000, 2000000);
@@ -28,6 +29,13 @@ namespace Team02.Scene.Stage
         {
             Player.CameraCenter = IGConfig.screen.ToVector2() / 2;
             base.Initialize();
+            testbutton = new HiddenBlock(this, "adfa")
+            {
+                UnitedSize = new Size(5, 5),
+                Coordinate = new Vector2(500, 500),
+            };
+
+            testbutton.Create();
         }
 
         public override void PreLoadContent()
@@ -39,6 +47,7 @@ namespace Team02.Scene.Stage
             testenemy = new Enemy(this, "enemy");
             testenemy.Coordinate = new Vector2(800, 400);
             testenemy.Size = new Size(64, 64);
+
             //Test
 
 
