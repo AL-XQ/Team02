e30d5da92bd62acf2ae002a4c4954d21c9bd2a88
tatsuya9034 <tatsuya.stoo@gmail.com>
2019-01-09 11:31:25 +0900

    EnemyCountUIとTimerUIの修正

--- Team02/Team02/Scene/UI/TimerUI.cs
+++ Team02/Team02/Scene/UI/TimerUI.cs
@@ -20,12 +20,15 @@ namespace Team02.Scene.UI
     public class TimerUI : Panel
     {
         private PlayScene playScene;
-        private Panel pa;
+        private Panel frontPa;
+        private Label numberLa;
+        private bool isTime;
         private float currentTime;
         private float limitTime;
 
         public float CurrentTime { get => currentTime; set => currentTime = value; }
         public float LimitTime { get => limitTime; set => limitTime = value; }
+        public bool IsTime { get => isTime; set => isTime = value; }
         public TimerUI(BaseDisplay parent) : base(parent)
         {
             playScene = (PlayScene)parent;
@@ -33,17 +36,22 @@ namespace Team02.Scene.UI
 
         public override void PreLoadContent()
         {
-            pa = new Panel(this);
-            pa.Location = new Point(200, 0);
-            Location = new Point(200,0);
+            frontPa = new Panel(this);
+            numberLa = new Label(this);
+            numberLa.BDText.ForeColor = System.Drawing.Color.Red;
             base.PreLoadContent();
         }
 
         public override void LoadContent()
         {
-            pa.Image = ImageManage.GetSImage("EnemyRateBack.png");
-            pa.Size = new Size(pa.Image.Size.Width, pa.Image.Size.Height);
             Image = ImageManage.GetSImage("EnemyRateFront.png");
+            Size = new Size(image.Size.Width, image.Size.Height);
+            Location = new Point(playScene.Size.Width - Size.Width, 0);
+            frontPa.Image = ImageManage.GetSImage("EnemyRateBack.png");
+            frontPa.Size = new Size(frontPa.Image.Size.Width, frontPa.Image.Size.Height);
+            frontPa.Location = Location;
+            numberLa.Location = new Point(0, Size.Height);
+            frontPa.Visible = false;
             SetTime(60);
             base.LoadContent();
         }
@@ -51,6 +59,11 @@ namespace Team02.Scene.UI
         public override void Update(GameTime gameTime)
         {
             currentTime = Math.Max(currentTime - 1.0f, 0.0f);
+            numberLa.Text = ((int)(currentTime / 60)).ToString();
+            if (currentTime <= 0.0f)
+            {
+                isTime = true;
+            }
             base.Update(gameTime);
         }
 
@@ -62,9 +75,9 @@ namespace Team02.Scene.UI
 
         public override void Draw1(GameTime gameTime)
         {
-            float rate = 1 - currentTime / limitTime;
-            //spriteBatch.Draw(pa.Image.ImageT[iTIndex], pa.Location.ToVector2(), Color.White);
-            spriteBatch.Draw(image.ImageT[iTIndex], Location.ToVector2(), null, Color.White, 0.0f, Vector2.Zero, new Vector2(1, rate), SpriteEffects.None, 1.0f);
+            float rate = currentTime / limitTime;
+            spriteBatch.Draw(Image.ImageT[iTIndex], Location.ToVector2(), Color.White);
+            spriteBatch.Draw(frontPa.Image.ImageT[iTIndex], frontPa.Location.ToVector2(), null, Color.White, 0.0f, Vector2.Zero, new Vector2(1, rate), SpriteEffects.None, 1.0f);
         }
     }
 }
