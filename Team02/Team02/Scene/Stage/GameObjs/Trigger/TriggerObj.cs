c1140d96706dc721e1dbc861b5ba1a3ce3079d66
AL-XQ <thealienxq@gmail.com>
2019-01-16 11:46:20 +0900

    追加

---
+++ Team02/Team02/Scene/Stage/GameObjs/Trigger/TriggerObj.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfinityGame.GameGraphics;
+
+namespace Team02.Scene.Stage.GameObjs.Trigger
+{
+    public abstract class TriggerObj : GameObj
+    {
+        public TriggerObj(BaseDisplay aParent) : base(aParent)
+        {
+        }
+
+        public TriggerObj(MapCreator mapCreator, Dictionary<string, object> args) : base(mapCreator, args)
+        {
+
+        }
+
+
+    }
+}
